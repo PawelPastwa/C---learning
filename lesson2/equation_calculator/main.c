f7d9e862486dde02d30c35c51f67b8bc098fdb1d
PawelPastwa <148966220+PawelPastwa@users.noreply.github.com>
2024-03-20 17:10:02 +0100

    Create main.c

---
+++ lesson2/equation_calculator/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "prototypy.h"
+#include "deklaracje.c"
+
+int main()
+{
+    float x;
+    float alfa;
+    float beta;
+    printf("Podaj x:\n");
+    scanf("%f", &x);
+    printf("Podaj alfe:\n");
+    scanf("%f", &alfa);
+    printf("Podaj bete:\n");
+    scanf("%f", &beta);
+    printf("Wynik rownania a to %f\n", a(x));
+    printf("wynik rownania b to %f\n", b(x));
+    printf("Wynik rownania c to %f\n", c(alfa));
+    printf("Wynik rownania d to %f\n", d(beta));
+
+
+
+
+    return 0;
+}
