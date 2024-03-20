f0c8bbfea959dd49d9e3d883f46a8843638a7c91
PawelPastwa <148966220+PawelPastwa@users.noreply.github.com>
2024-03-20 16:03:24 +0100

    Create height_dyscrimination_program.c

---
+++ lesson1/height_dyscrimination_program.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //deklaracje
+
+    int lstudentow;
+    float wzrost;
+
+    //instrukcje
+
+    printf("Podaj liczbe osob\n");
+    scanf("%d", &lstudentow);
+
+    while (lstudentow != 0)
+    {
+        printf("podaj wzrost\n");
+        scanf("%f", &wzrost);
+
+        if (wzrost < 170)
+        {
+            printf("Niski wzrost\n\n");
+        }
+        else if (wzrost < 183)
+        {
+            printf("sredni wzrost\n\n");
+        }
+        else
+        {
+            printf("wysoki wzrost\n\n");
+        }
+        lstudentow--;
+    }
+
+    return 0;
+}
