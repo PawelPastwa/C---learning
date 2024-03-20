b8d210843e5260df0d9787e89f2e1b32c2c1c715
PawelPastwa <148966220+PawelPastwa@users.noreply.github.com>
2024-03-20 15:54:08 +0100

    Create field_volume_calculator.c

---
+++ lesson1/field_volume_calculator.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //deklaracje
+
+    float a;
+    float b;
+    float c;
+    float d;
+    float e;
+    float f;
+    float g;
+    float os;
+    float pps;
+    float op;
+    float ppp;
+    float ppk;
+    float ok;
+    float ow;
+    float ppw;
+    float pi = 3.14;
+
+
+    //instrukcje
+
+    printf("Podaj dlugosc boku szescianu\n");
+    scanf("%f", &a);
+    os = a*a*a;
+    pps = a*a*6;
+
+    printf("Podaj dlugosc bokow prostopadloscianu\n");
+    scanf("%f %f %f", &b, &c, &d);
+    op = b*c*d;
+    ppp = b*c*2 + b*d*2 + c*d*2;
+
+    printf("Podaj promien kuli\n");
+    scanf("%f", &e);
+    ppk = 4*pi*e*e;
+    ok = (4/3)*pi*e*e*e;
+
+    printf("Podaj promien i wysokosc walca\n");
+    scanf("%f %f", &f, &g);
+    ow = pi*(f*f)*g;
+    ppw = 2*pi*f*g + 2*pi*(f*f);
+
+    printf("Objetosc szescianu wynosi:\n%f\nPole powierzchni szescianu wynosi:\n%f\n\nObjetosc prostopadloscianu wynosi:\n%f\nPolepowierzchni prostopadloscianu wynosi:\n%f\n\nObjetosc kuli wynosi:\n%f\nPole powierzcni kuli wynosi:\n%f\n\nObjetosc walca wynosi:\n%f\nPole powierzchni walca wynosi:\n%f", os, pps, op, ppp, ok, ppk, ow, ppw);
+
+    return 0;
+}
