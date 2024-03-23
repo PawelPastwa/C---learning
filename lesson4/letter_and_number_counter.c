e41ad2a2bd8cc3e9764c1b0137106365693160c5
PawelPastwa <148966220+PawelPastwa@users.noreply.github.com>
2024-03-23 12:24:09 +0100

    Create letter_and_number_counter.c

---
+++ lesson4/letter_and_number_counter.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//prototyp funkcji
+
+void ileznakow();
+
+int main()
+{
+    ileznakow();
+    return 0;
+}
+
+//deklaracja funkcji
+
+void ileznakow()
+{
+    char litera, znak;
+    int ilecyfr = 0, ileliter = 0;
+
+    printf("Podaj litere ktorej szukasz:\n");
+    scanf("%c", &litera);
+    printf("Wpisz zdanie zakonczone kropka:\n");
+
+    do
+    {
+        scanf("%c", &znak);
+        if (znak >= '0' && znak <= '9') ilecyfr++;
+        if (znak == litera) ileliter++;
+    }
+    while (znak != '.');
+
+    printf("Litera '%c' wystapila w zdaniu %d razy, liczba cyfr: %d\n", litera, ileliter, ilecyfr);
+
+
+
+
+}
