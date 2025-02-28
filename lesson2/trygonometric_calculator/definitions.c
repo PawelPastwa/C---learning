23bc162a48d9640dba1cdd853a1962f1dd7217a7
PawelPastwa <148966220+PawelPastwa@users.noreply.github.com>
2025-02-28 17:54:37 +0100

    Delete lesson2/trygonometric_calculator/definitions.c

--- lesson2/trygonometric_calculator/definitions.c
+++
@@ -1,8 +0,0 @@
-#include <math.h>
-float funkcje_tryg(float alfa, int funkcja)
-{
-    if (funkcja == 0) return sin((alfa/30)*(M_PI/6));
-    else if (funkcja == 1) return cos((alfa/30)*(M_PI/6));
-    else if (funkcja == 2) return tan((alfa/30)*(M_PI/6));
-    else printf("Bledna wartosc\n");
-}
