365ced4b8fc14b12f7c87b6003a0dfac2d64c37a
PawelPastwa <148966220+PawelPastwa@users.noreply.github.com>
2025-02-28 14:53:10 +0100

    Update field_volume_calculator.c

--- lesson1/field_volume_calculator.c
+++ lesson1/field_volume_calculator.c
@@ -46,7 +46,7 @@ int main()
     ow = M_PI*(f*f)*g;
     ppw = 2*M_PI*f*g + 2*M_PI*(f*f);
 
-    printf("Objetosc szescianu wynosi:\n%f\nPole powierzchni szescianu wynosi:\n%f\n\nObjetosc prostopadloscianu wynosi:\n%f\nPolepowierzchni prostopadloscianu wynosi:\n%f\n\nObjetosc kuli wynosi:\n%f\nPole powierzcni kuli wynosi:\n%f\n\nObjetosc walca wynosi:\n%f\nPole powierzchni walca wynosi:\n%f", os, pps, op, ppp, ok, ppk, ow, ppw);
+    printf("Objetosc szescianu wynosi:\n%f\nPole powierzchni szescianu wynosi:\n%f\n\nObjetosc prostopadloscianu wynosi:\n%f\nPole powierzchni prostopadloscianu wynosi:\n%f\n\nObjetosc kuli wynosi:\n%f\nPole powierzcni kuli wynosi:\n%f\n\nObjetosc walca wynosi:\n%f\nPole powierzchni walca wynosi:\n%f", os, pps, op, ppp, ok, ppk, ow, ppw);
 
     return 0;
 }
