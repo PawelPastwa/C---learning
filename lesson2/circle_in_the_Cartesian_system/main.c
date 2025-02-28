c87f5ba9441c1e5317a55ae8e391e61291298e74
PawelPastwa <148966220+PawelPastwa@users.noreply.github.com>
2025-02-28 15:28:53 +0100

    Update main.c

--- lesson2/circle_in_the_Cartesian_system/main.c
+++ lesson2/circle_in_the_Cartesian_system/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "prototypy.h"
-#include "definicje.c"
+#include <math.h>
+
+float ok(float x1, float y1, float x2, float y2);
 
 int main()
 {
@@ -14,4 +15,13 @@ int main()
 
     printf("Obwod okregu o wspolrzednych srodka: %f %f, przechodzacego przez punkt: %f %f\nwynosi: %f", x1, y1, x2, y2, ok(x1, y1, x2, y2));
     return 0;
+}
+
+float ok(float x1, float y1, float x2, float y2)
+{
+    float promien, ok;
+
+    promien = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+
+    return 2*M_PI*promien;
 }
