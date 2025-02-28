f40e831bdc5729330c3cddb84b56220c051124ea
PawelPastwa <148966220+PawelPastwa@users.noreply.github.com>
2025-02-28 15:13:27 +0100

    Update quadratic_equation.c

--- lesson1/quadratic_equation.c
+++ lesson1/quadratic_equation.c
@@ -3,8 +3,7 @@
 #include <math.h>
 
 
-int main()
-{
+int main() {
     //deklaracje
     float a;
     float b;
@@ -16,31 +15,21 @@ int main()
     //instrukcje
     printf("Podaj wspolczynniki a, b i c wzor: ax^2 + bx +c\n");
     scanf("%f %f %f", &a, &b, &c);
-    if (a == 0)
-    {
+    if (a == 0) {
         printf("to nie jest funkcja kwadratowa");
+    } else {
+        delta = (b * b) - (4 * a * c);
+        if (delta > 0) {
+            x1 = ((-b - sqrt(delta)) / (2 * a));
+            x2 = ((-b + sqrt(delta)) / (2 * a));
+            printf("Rownanie ma 2 pierwiastki:\nx1 = %f\nx2 = %f", x1, x2);
+        } else if (delta == 0) {
+            x1 = (-b) / (2 * a);
+            x2 = x1;
+            printf("Rownanie ma jeden pierwiastek\nx = %f", x1);
+        } else {
+            printf("To rownanie nie ma pierwiastkow rzeczywistych");
+        }
     }
-    else
-    {
-    delta = (b*b) - (4*a*c);
-    if (delta > 0)
-    {
-        x1 = ((-b - sqrt(delta))/(2*a));
-        x2 = ((-b + sqrt(delta))/(2*a));
-        printf("Rownanie ma 2 pierwiastki:\nx1 = %f\nx2 = %f", x1, x2);
-    }
-    else if (delta == 0)
-    {
-        x1 = (-b)/(2*a);
-        x2 = x1;
-        printf("Rownanie ma jeden pierwiastek\nx = %f", x1);
-    }
-    else
-    {
-        printf("To rownanie nie ma pierwiastkow rzeczywistych");
-    }
-    }
-
-
     return 0;
 }
