97ccf81e9e688c9c8f1e8c287424ffa6dcdb03d3
PawelPastwa <148966220+PawelPastwa@users.noreply.github.com>
2025-03-03 15:13:23 +0100

    Update avarage_odd_even.c

--- lesson4/avarage_odd_even.c
+++ lesson4/avarage_odd_even.c
@@ -2,34 +2,35 @@
 
 void sredniaPiNP(int liczba);
 
-int main()
-{
+int main() {
     int liczba;
     sredniaPiNP(liczba);
     return 0;
 }
-void sredniaPiNP(int liczba)
-{
+
+void sredniaPiNP(int liczba) {
     int licznikp = 0, liczniknp = 0;
     float sumap = 0, sumanp = 0, sredniap = 0, srednianp = 0;
 
-    do
-    {
+    do {
         printf("Podaj liczbe:\n");
         scanf("%d", &liczba);
-        if (liczba % 2 == 0 && liczba != 0)
-        {
+        if (liczba % 2 == 0 && liczba != 0) {
             sumap += liczba;
             licznikp++;
-        }
-        else if(liczba % 2 != 0)
-        {
+        } else if (liczba % 2 != 0) {
             sumanp += liczba;
             liczniknp++;
         }
-    }
-    while (liczba != 0);
-    sredniap = sumap/licznikp;
-    srednianp = sumanp/liczniknp;
-    printf("Srednia liczb parzystych wynosi: %f\nSrednia liczb nieparzystych wynosi: %f", sredniap, srednianp);
+    } while (liczba != 0);
+    sredniap = sumap / licznikp;
+    srednianp = sumanp / liczniknp;
+    if (sumap == 0 && sumanp == 0)
+        printf("Nie podano zadnych liczb\n");
+    else if (sumap == 0)
+        printf("Nie podano liczb parzystych\nSrednia liczb nieparzystych wynosi: %f\n", srednianp);
+    else if (sumanp == 0)
+        printf("Srednia liczb parzystych wynosi: %f\nNie podano liczb nieparzystych\n", sredniap);
+    else
+        printf("Srednia liczb parzystych wynosi: %f\nSrednia liczb nieparzystych wynosi: %f\n", sredniap, srednianp);
 }
