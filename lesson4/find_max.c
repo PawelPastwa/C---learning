505fa9bcb685d6a172e840db72927910e251922e
PawelPastwa <148966220+PawelPastwa@users.noreply.github.com>
2025-06-20 10:29:19 +0200

    Update find_max.c

--- lesson4/find_max.c
+++ lesson4/find_max.c
@@ -16,7 +16,7 @@ void najwieksza()
     while (liczba != -1)
     {
         printf("Podaj liczbe calkowita (-1 by porownac):\n");
-        scanf("%d", &liczba);
+        scanf("%f", &liczba);
         if (liczba > top) top = liczba;
     }
     printf("najwieksza liczba sposrod podanych wynosi: %f", top);
