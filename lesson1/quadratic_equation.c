e8ff6f9d4408d4ddc2232269b62d4541bbbcb2cb
PawelPastwa <148966220+PawelPastwa@users.noreply.github.com>
2025-08-14 16:54:29 +0200

    Update quadratic_equation.c

--- lesson1/quadratic_equation.c
+++ lesson1/quadratic_equation.c
@@ -15,19 +15,25 @@ int main() {
     //instrukcje
     printf("Podaj wspolczynniki a, b i c wzor: ax^2 + bx +c\n");
     scanf("%f %f %f", &a, &b, &c);
-    if (a == 0) {
+    if (a == 0) 
         printf("to nie jest funkcja kwadratowa");
-    } else {
+    else 
+    {
         delta = (b * b) - (4 * a * c);
-        if (delta > 0) {
+        if (delta > 0) 
+        {
             x1 = ((-b - sqrt(delta)) / (2 * a));
             x2 = ((-b + sqrt(delta)) / (2 * a));
             printf("Rownanie ma 2 pierwiastki:\nx1 = %f\nx2 = %f", x1, x2);
-        } else if (delta == 0) {
+        } 
+        else if (delta == 0) 
+        {
             x1 = (-b) / (2 * a);
             x2 = x1;
             printf("Rownanie ma jeden pierwiastek\nx = %f", x1);
-        } else {
+        } 
+        else 
+        {
             printf("To rownanie nie ma pierwiastkow rzeczywistych");
         }
     }
