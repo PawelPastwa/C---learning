7f1e89cfe1b0a4a676596d0fde24bf95409cd1b4
PawelPastwa <148966220+PawelPastwa@users.noreply.github.com>
2025-08-14 17:07:49 +0200

    Update average_grade.c

--- lesson1/average_grade.c
+++ lesson1/average_grade.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+
 
 int main()
 {
@@ -9,32 +8,28 @@ int main()
     int n;
     float s = 0;
     float x;
-    int n0;
 
     //instrukcje
 
-    printf("Podaj liczbe egzaminow\n");
+    printf("Podaj liczbe egzaminow:\n");
     scanf("%d", &n);
+    while (n <= 0)
+    {
+        printf("Podaj poprawna liczbe egzaminow:\n");
+        scanf("%d", &n);
+    }
 
-    n0 = n;
+    int n0 = n;
 
     while(n != 0)
-        {
+    {
         printf("Podaj ocene z egzaminu\n");
         scanf("%f", &x);
-        s = s + x;
-        n -= 1;
-        }
+        s += x;
+        n--;
+    }
 
-    if (n0 != 0)
-    {
-        s = s / n0;
-        printf("Srednia z egzaminow wynosi:\n %f", s);
-    }
-    else
-    {
-        printf("Nie podano zadnych occen");
-    }
+    printf("Srednia z egzaminow wynosi: %f", s/n0);
 
     return 0;
 }
