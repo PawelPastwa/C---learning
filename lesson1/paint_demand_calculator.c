4fc3da62d3a5579564e798f45995057d29d5e5b9
PawelPastwa <148966220+PawelPastwa@users.noreply.github.com>
2025-08-14 17:52:12 +0200

    Update paint_demand_calculator.c

--- lesson1/paint_demand_calculator.c
+++ lesson1/paint_demand_calculator.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
 
 int main()
 {
     //deklaracje
-    //including doors and window in the room, window 1x0.9, door 2x0.8
+    //including door and window in the room, window 1x0.9, door 2x0.8
 
     float a;
     float b;
     float c;
-    float d;
     float sciany;
     float farba;
     float zapotrzebowanie;
@@ -19,14 +17,24 @@ int main()
 
     printf("Podaj wymiar pierwszy, wymiar drugi i wysokosc pokoju\n");
     scanf("%f %f %f", &a, &b, &c);
-    sciany = a*c*2 + b*c*2 - (1*0.9) - (2*0.8);
+    while (a <= 0 || b <= 0 || c <= 0)
+    {
+        printf("Podaj poprawne wymiary:\n");
+        scanf("%f %f %f", &a, &b, &c);
+    }
+    sciany = 2*c*(a+b) - (1*0.9) - (2*0.8);
 
     printf("Podaj wartosc w m^2 na ile starcza 1 litr puszki farby\n");
     scanf("%f", &farba);
+    while (farba <= 0)
+    {
+        printf("Podaj poprawna wartosc:\n");
+        scanf("%f", &farba);
+    }
     zapotrzebowanie = sciany/farba;
-    zapotrzebowanie = ceil(zapotrzebowanie);
+    zapotrzebowanie = ceilf(zapotrzebowanie);
 
-    printf("Na pomalowanie pokoju potrzeba:\n%f puszek farby", zapotrzebowanie);
+    printf("Na pomalowanie pokoju potrzeba:\n%.0f puszek farby", zapotrzebowanie);
 
     return 0;
 }
