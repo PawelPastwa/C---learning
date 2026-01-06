38d5f0cef82c3f1581a436d93ccba28dc81f0b61
PawelPastwa <148966220+PawelPastwa@users.noreply.github.com>
2026-01-06 22:05:20 +0100

    Update cosmetics_console.c

--- lesson5/cosmetics_console.c
+++ lesson5/cosmetics_console.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-//prototyp funkcji
 void szlaczek(int ile, char znak);
 
 void szlaczekv2(int ilew, int ilek, char znakk);
@@ -9,15 +8,35 @@ int main() {
 
     int q, r;
     char s;
-    printf("Podaj ile znakow, ile wierszy jaki znak\n");
-    scanf("%d %d %c", &q, &r, &s);
-    szlaczekv2(q, r, s);
+
+    printf("Podaj ile znakow i jaki znak\n");
+    scanf("%d %c", &q, &s);
+    while (q <= 0) {
+        printf("Podaj poprawna liczbe znakow >0\n");
+        scanf("%d", &q);
+    }
     szlaczek(q, s);
+    printf("Podaj liczbe kolumn\n");
+    scanf("%d", &q);
+    while (q <= 0){
+        printf("Podaj poprawna liczbe kolumn\n");
+        scanf("%d", &q);
+    }
+    printf("Podaj liczbe wierszy\n");
+    scanf("%d", &r);
+    while (r <= 0){
+        printf("Podaj poprawna liczbe wierszy\n");
+        scanf("%d", &r);
+    }
+    getchar();
+    printf("Podaj znak\n");
+    scanf( "%c", &s);
+
+    szlaczekv2(r, q, s);
 
     return 0;
 }
 
-//definicja funkcji
 void szlaczek(int ile, char znak)
 {
     int i;
@@ -32,9 +51,9 @@ void szlaczekv2(int ilew, int ilek, char znakk)
 {
     int i, h;
 
-    for (h = 1; h <= ilek; h++)
+    for (h = 1; h <= ilew; h++)
     {
-        for (i = 1; i <= ilew; i++)
+        for (i = 1; i <= ilek; i++)
         {
             printf("%c", znakk);
         }
